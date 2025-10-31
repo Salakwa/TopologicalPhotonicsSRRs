fd7e95c833734c7cc970ceba214e63d8a10acb0d
Salakwa <tommysalako44@gmail.com>
2025-10-31 12:07:15 -0400

    Start of Bloch Eigenmode plotting

---
+++ BlochEigenmodes.py
@@ -0,0 +1,28 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+# Start w/ Our Saved Values Here
+# These arry of ratios represent the value of J2/J1
+J1 = 2
+J2 = 1
+ratios = [0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2]
+
+
+# Value of K (thE
+wavevector_k = (2 * math.pi * n)
+
+
+
+
+bandgap_delta = 2 * abs(J1-J2)
+
+
+
+# Band should close at J2/J1 = 1
+#
+
+
+
